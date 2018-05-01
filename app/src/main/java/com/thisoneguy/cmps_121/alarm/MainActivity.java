819fd30fb92e87e9beb03da6c557e7ee8a99e339
thisoneguy365 <kirangunasekaran@yahoo.ocm>
2018-05-01 13:15:17 -0700

    Holla

---
+++ app/src/main/java/com/thisoneguy/cmps_121/alarm/MainActivity.java
@@ -0,0 +1,98 @@
+package com.thisoneguy.cmps_121.alarm;
+
+import android.app.AlarmManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    // Alarm Manager
+    AlarmManager alarm_manager;
+    TimePicker alarm_timepicker;
+    TextView update_text;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        this.context = this;
+
+        // initialize our alarm manager
+        alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        // initialize all our time picker
+        alarm_timepicker = (TimePicker) findViewById(R.id.timePicker);
+
+        // initialize text update box
+        update_text = (TextView) findViewById(R.id.Update_Alarm);
+
+        // create an instance of a calendar
+        Calendar calendar = Calendar.getInstance();
+
+        // initialize Set Alarm
+        Button alarm_on = (Button) findViewById(R.id.Set_Alarm);
+        // create an onClickListener to start the alarm
+        alarm_on.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // method the changes the update text Textbox
+            }
+        });
+
+
+
+
+
+
+        // initialize Cancel Alarm
+        Button alarm_off = (Button) findViewById(R.id.End_Alarm);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
